8f999d40f293bbbffbd5140a0e8b538db897d8ee
Subhradeep333 <codewithsubhradeep3@gmail.com>
2026-09-23 01:42:28 +0530

    Fix ESP32 Core 3.x LEDC API compatibility and remove stray # comments in config.h

--- esp32/config.h
+++ esp32/config.h
@@ -11,8 +11,8 @@
 // Wi-Fi Access Point Credentials & RF Parameters
 #define WIFI_SSID "GestureCar"
 #define WIFI_PASS "12345678"
-#define WIFI_CHANNEL 1       # Dedicated 2.4GHz Wi-Fi Channel 1 (Low Interference)
-#define MAX_AP_CLIENTS 1     # Reject external probing devices to dedicate 100% bandwidth to Laptop
+#define WIFI_CHANNEL 1       // Dedicated 2.4GHz Wi-Fi Channel 1 (Low Interference)
+#define MAX_AP_CLIENTS 1     // Reject external probing devices to dedicate 100% bandwidth to Laptop
 
 // Network IP Parameters
 #define AP_IP_1 192
--- esp32/motor_controller.cpp
+++ esp32/motor_controller.cpp
@@ -1,5 +1,6 @@
 #include "motor_controller.h"
 #include "config.h"
+#include <esp_arduino_version.h>
 
 void initMotors() {
     pinMode(IN1_PIN, OUTPUT);
@@ -7,19 +8,33 @@ void initMotors() {
     pinMode(IN3_PIN, OUTPUT);
     pinMode(IN4_PIN, OUTPUT);
 
-    // Setup ESP32 LEDC PWM Hardware Channels for ENA and ENB
+#if ESP_ARDUINO_VERSION >= ESP_ARDUINO_VERSION_VAL(3, 0, 0)
+    // ESP32 Arduino Core 3.x API
+    ledcAttach(ENA_PIN, PWM_FREQ, PWM_RESOLUTION);
+    ledcAttach(ENB_PIN, PWM_FREQ, PWM_RESOLUTION);
+#else
+    // ESP32 Arduino Core 2.x API
     ledcSetup(PWM_CHANNEL_ENA, PWM_FREQ, PWM_RESOLUTION);
     ledcAttachPin(ENA_PIN, PWM_CHANNEL_ENA);
-
     ledcSetup(PWM_CHANNEL_ENB, PWM_FREQ, PWM_RESOLUTION);
     ledcAttachPin(ENB_PIN, PWM_CHANNEL_ENB);
+#endif
 
     stopMotors();
 }
 
+static inline void setPwm(uint8_t speed_ena, uint8_t speed_enb) {
+#if ESP_ARDUINO_VERSION >= ESP_ARDUINO_VERSION_VAL(3, 0, 0)
+    ledcWrite(ENA_PIN, speed_ena);
+    ledcWrite(ENB_PIN, speed_enb);
+#else
+    ledcWrite(PWM_CHANNEL_ENA, speed_ena);
+    ledcWrite(PWM_CHANNEL_ENB, speed_enb);
+#endif
+}
+
 void moveForward(uint8_t speed) {
-    ledcWrite(PWM_CHANNEL_ENA, speed);
-    ledcWrite(PWM_CHANNEL_ENB, speed);
+    setPwm(speed, speed);
 
     digitalWrite(IN1_PIN, HIGH);
     digitalWrite(IN2_PIN, LOW);
@@ -29,8 +44,7 @@ void moveForward(uint8_t speed) {
 }
 
 void moveBackward(uint8_t speed) {
-    ledcWrite(PWM_CHANNEL_ENA, speed);
-    ledcWrite(PWM_CHANNEL_ENB, speed);
+    setPwm(speed, speed);
 
     digitalWrite(IN1_PIN, LOW);
     digitalWrite(IN2_PIN, HIGH);
@@ -40,8 +54,7 @@ void moveBackward(uint8_t speed) {
 }
 
 void turnLeft(uint8_t speed) {
-    ledcWrite(PWM_CHANNEL_ENA, speed);
-    ledcWrite(PWM_CHANNEL_ENB, speed);
+    setPwm(speed, speed);
 
     // Skid Steer Left: Left wheels reverse, Right wheels forward
     digitalWrite(IN1_PIN, LOW);
@@ -52,8 +65,7 @@ void turnLeft(uint8_t speed) {
 }
 
 void turnRight(uint8_t speed) {
-    ledcWrite(PWM_CHANNEL_ENA, speed);
-    ledcWrite(PWM_CHANNEL_ENB, speed);
+    setPwm(speed, speed);
 
     // Skid Steer Right: Left wheels forward, Right wheels reverse
     digitalWrite(IN1_PIN, HIGH);
@@ -64,8 +76,7 @@ void turnRight(uint8_t speed) {
 }
 
 void stopMotors() {
-    ledcWrite(PWM_CHANNEL_ENA, 0);
-    ledcWrite(PWM_CHANNEL_ENB, 0);
+    setPwm(0, 0);
 
     digitalWrite(IN1_PIN, LOW);
     digitalWrite(IN2_PIN, LOW);
